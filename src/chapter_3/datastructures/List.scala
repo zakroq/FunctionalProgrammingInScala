68af465382cf8a21fd2a2a1b06217ec14ddf4e87
zaq <zaq@mp3.pl>
2019-11-11 12:29:38 +0100

    LEARN-42

--- src/chapter_3/datastructures/List.scala
+++ src/chapter_3/datastructures/List.scala
@@ -1,5 +1,26 @@
 package chapter_3.datastructures
 
-class List {
+sealed trait List[+A] // List data type, parametrized on a type, A
+case object Nil extends List[Nothing] // A List data constructor representing the empty list
+case class Cons[+A](head: A, tail: List[A]) extends List[A] // Another data constructor, representing nonempty lists.
+                                                            // Note that 'tail' is another List[A], which may be Nil or another Cons
+
+object List { // List companion object, contains functions for creating and working with lists
+  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+    case Nil => 0 // The sum of the empty list is 0
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with 'x' is 'x' plus the sum of the rest of the list
+  }
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
 
 }
+
+
